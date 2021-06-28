c8eb04ef9b3e6676fc4845e4702993479c34905e
datcit241 <85815364+datcit241@users.noreply.github.com>
2021-06-28 10:14:03 +0700

    Add files via upload

---
+++ CSES/CoinCombinations.java
@@ -0,0 +1,137 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class CoinCombinations {
+
+	public static void main(String[] args) {
+		int n = ni();
+		int sum = ni();
+
+		int[] coins = new int[n];
+
+		long[] combinationsOfCoins = new long[sum + 1];
+
+		for (int i = 0; i < n; i++) {
+			int val = ni();
+			
+			coins[i] = val;
+			
+			if (val <= sum) {
+				combinationsOfCoins[val] = 1;
+			}
+		}
+		for (int i = 0; i <= sum; i++) {
+			for (int j = 0; j < n; j++) {
+				int coin = coins[j];
+				if (i > coin) {
+					combinationsOfCoins[i] = (combinationsOfCoins[i] + combinationsOfCoins[i - coin]) % 1000000007;
+				}
+			}
+		}
+		System.out.println(combinationsOfCoins[sum]);
+
+	}
+
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+}
