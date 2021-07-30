e59cc7c841ddb61868a9d37457950533165dc2a1
datcit241 <85815364+datcit241@users.noreply.github.com>
2021-07-30 11:19:20 +0700

    Add files via upload

---
+++ CSES/SubarraySumsII.java
@@ -0,0 +1,148 @@
+import java.io.*;
+import java.util.*;
+
+public class SubarraySumsII {
+	
+	public static void main(String[] args) {
+		int n = ni(), x = ni();
+		
+		Map<Long, Integer> map = new HashMap<>();
+		map.put(0l, 1);
+		
+		long sum = 0;
+		long count = 0;
+		
+		for (int i = 0; i < n; i++) {
+			sum += ni();
+			
+			long complement = sum - x;
+			
+			if (map.containsKey(complement)) {
+				count+= map.get(complement);
+			}
+			
+			if (map.containsKey(sum)) {
+				map.put(sum, map.get(sum) + 1);
+			} else {
+				map.put(sum, 1);
+			}
+			
+		}
+		System.out.print(count);
+		
+		
+	}
+	
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+	
+	static boolean isSpaceChar2(int c) {
+		return !(c >= 32 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	static String nextLine() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar2(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+}
